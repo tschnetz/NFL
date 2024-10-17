05d6b9cbe0f96780237119e56b3d36b8e368c973
Tom Schnetzer <160368069+tschnetz@users.noreply.github.com>
2024-10-17 19:46:27 -0400

    v3 - Added background and more css

--- google-deploy.sh
+++ google-deploy.sh
@@ -3,7 +3,7 @@
   #	•	Go to the project selector and click New Project.
   #	•	Give your project a name and note down the project ID.
 # Create a Dockerfile in the root of your project directory
-docker build -t nfl-app .
+docker build --platform linux/amd64 -t nfl-app .
 # gcloud auth login
 gcloud config set project nfl-data-2024
 gcloud builds submit --tag gcr.io/nfl-data-2024/nfl-app
