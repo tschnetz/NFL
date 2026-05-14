3d7d2530b1fccf81e65257a66f5acadd32ac0562
Tom Schnetzer <160368069+tschnetz@users.noreply.github.com>
2026-05-13 22:04:19 -0400

    Initial Commit

---
+++ NFL/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NFL
+//
+//  Created by T G Schnetzer on 5/13/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NFL/NFLApp.swift
@@ -0,0 +1,17 @@
+//
+//  NFLApp.swift
+//  NFL
+//
+//  Created by T G Schnetzer on 5/13/26.
+//
+
+import SwiftUI
+
+@main
+struct NFLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
